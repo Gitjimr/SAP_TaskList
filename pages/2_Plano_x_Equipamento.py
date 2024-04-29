089464c6ddd183bc5041e10edf7085fc5b7284b6
Gitjimr <166082003+Gitjimr@users.noreply.github.com>
2024-04-29 16:07:49 -0300

    Add files via upload

--- pages/2_Plano_x_Equipamento.py
+++ pages/2_Plano_x_Equipamento.py
@@ -81,171 +81,170 @@ if uploaded_file is not None or 'SAP_CTPM' in st.session_state:
         SAP_MATERIAIS = st.session_state['SAP_MATERIAIS']
 
 
-    st.header('Identificar Equipamentos com Planos Carregados', divider='gray', anchor=False)
+    st.header('Identificar Equipamentos Principais com Planos Carregados', divider='gray', anchor=False)
+
+    with st.spinner("Carregando..."):
+
+        # Leitura dos arquivos baixados
+
+        ia39 = SAP_TL
+        ip18 = SAP_ITEM
+
+        ia39 = ia39[['Equipamento operação', 'Descrição','Grupo']].copy()
+        ia39.rename(columns={'Equipamento operação': 'ID Equipamento'}, inplace = True)
+        ip18 = ip18[['Plano de manutenção', 'Grupo', 'Equipamento','Descrição item de manutenção']].copy()
+        ip18 = ip18[['Equipamento','Descrição item de manutenção','Grupo','Plano de manutenção']].copy()
+        ip18.rename(columns={'Equipamento': 'ID Equipamento'}, inplace = True)
+        ip18.rename(columns={'Descrição item de manutenção': 'Descrição'}, inplace = True)
 
 
-    # Leitura dos arquivos baixados
+        #   SEPARAR EQUIPAMENTOS, LISTAS E PLANOS
 
-    ia39 = SAP_TL
-    ip18 = SAP_ITEM
+        # Criar uma coluna 'Plano de manutenção' no DataFrame ia39 preenchida com valores NaN
+        ia39['Plano de manutenção'] = np.nan
 
+        # Criar um dicionário a partir dos valores de 'Grupo' como chave e os valores correspondentes de 'Plano de manutenção' como valor
+        grupo_plano_dict = ip18.set_index('Grupo')['Plano de manutenção'].to_dict()
 
-    ia39 = ia39[['Equipamento operação', 'Grupo']].copy()
-    ia39.rename(columns={'Equipamento operação': 'EQP_N6'}, inplace = True)
+        # Mapear os valores de 'Grupo' de ia39 para os valores correspondentes de 'Plano de manutenção' usando o dicionário
+        ia39['Plano de manutenção'] = ia39['Grupo'].map(grupo_plano_dict)
 
-
-    st.write(ia39)
-    st.write(ip18)
-
-    #   SEPARAR EQUIPAMENTOS, LISTAS E PLANOS
-
-    # Criar uma coluna 'Plano de manutenção' no DataFrame ia39 preenchida com valores NaN
-    ia39['Plano de manutenção'] = np.nan
-
-    # Criar um dicionário a partir dos valores de 'Grupo' como chave e os valores correspondentes de 'Plano de manutenção' como valor
-    grupo_plano_dict = ip18.set_index('Grupo')['Plano de manutenção'].to_dict()
-
-    # Mapear os valores de 'Grupo' de ia39 para os valores correspondentes de 'Plano de manutenção' usando o dicionário
-    ia39['Plano de manutenção'] = ia39['Grupo'].map(grupo_plano_dict)
-
-    st.write(ia39)
-    st.write(ip18)
-
-    eqp_tl_plan = pd.concat([ip18, ia39], ignore_index=True, sort=False)
-    eqp_tl_plan.drop_duplicates()
-    eqp_tl_plan.dropna(subset=['EQP_N6'], inplace=True)
-    eqp_tl_plan.reset_index(drop=True, inplace=True)
-
-    st.write(eqp_tl_plan)
+        # Caso não encontrado
+        ia39['Plano de manutenção'] = ia39['Plano de manutenção'].fillna('SEM PLANO')
 
 
-    #   CRIAR E FORMATAR DF COM BASE NA BASE DE EQUIPAMENTOS
-
-    df_plan_eqp = SAP_EQP_N6
-    #df_plan_eqp['TEM PLANO?'] = df_plan_eqp['EQUIPAMENTO PRINCIPAL'].apply(lambda x: 'SIM' if x in eqp_tl_plan['EQP_N6'].values else 'SEM PLANO')
-    df_plan_eqp['TEM PLANO?'] = df_plan_eqp['ID_SAP_N6'].isin(eqp_tl_plan['EQP_N6']).map(
-        {True: 'SIM', False: 'SEM PLANO'})
-    df_plan_eqp['GRUPO'] = df_plan_eqp['LI_N5'].apply(lambda x: str(x)[9:12])
-    df_plan_eqp['PLANTA'] = df_plan_eqp['LI_N5'].apply(lambda x: str(x)[0:4])
-
-    df_plan_eqp['PLANO'] = np.nan
-    df_plan_eqp['LT'] = np.nan
+        eqp_tl_plan = pd.concat([ip18, ia39], ignore_index=True, sort=False)
+        eqp_tl_plan.drop_duplicates()
+        eqp_tl_plan.dropna(subset=['ID Equipamento'], inplace=True)
+        eqp_tl_plan.reset_index(drop=True, inplace=True)
 
 
-    #   TRAZER PARA CADA EQP OS PLANOS E LISTAS
+        # Removendo equipamentos não principais do eqp_tl_plan
+        equipamentos_principais = SAP_EQP_N6['ID_SAP_N6']
+        eqp_tl_plan = eqp_tl_plan[eqp_tl_plan['ID Equipamento'].isin(equipamentos_principais)]
 
-    # Converter os valores np.nan para listas vazias
-    eqp_tl_plan['Plano de manutenção'] = eqp_tl_plan['Plano de manutenção'].apply(
-        lambda x: '' if isinstance(x, float) and np.isnan(x) else x)
-    eqp_tl_plan['Grupo'] = eqp_tl_plan['Grupo'].apply(
-        lambda x: '' if isinstance(x, float) and np.isnan(x) else x)
+        # Identificando equipamentos principais que não estão no eqp_tl_plan
+        equipamentos_principais_faltantes = SAP_EQP_N6['ID_SAP_N6']
+        equipamentos_principais_faltantes = equipamentos_principais_faltantes[~equipamentos_principais_faltantes.isin(eqp_tl_plan['ID Equipamento'])]
 
-    # Agrupar e unir os planos de manutenção e grupos para cada equipamento em eqp_tl_plan
-    eqp_tl_plan_grouped = eqp_tl_plan.groupby('EQP_N6').agg({
-        'Plano de manutenção': lambda x: ','.join(map(str, x)),
-        'Grupo': lambda x: ','.join(map(str, x))
-    }).reset_index()
+        # Novo DataFrame com as linhas a serem adicionadas
+        novas_linhas = pd.DataFrame({
+            'ID Equipamento': equipamentos_principais_faltantes,
+            'Descrição': 'N/A',
+            'Grupo': 'SEM LISTA',
+            'Plano de manutenção': 'SEM PLANO'
+        })
 
-    # Juntar os DataFrames usando merge
-    df_plan_eqp = pd.merge(df_plan_eqp, eqp_tl_plan_grouped, how='left', left_on='ID_SAP_N6', right_on='EQP_N6')
-
-    # Preencher os valores nulos nas colunas 'PLANO' e 'LT' com '-'
-    df_plan_eqp['PLANO'] = df_plan_eqp['Plano de manutenção'].fillna('-')
-    df_plan_eqp['LT'] = df_plan_eqp['Grupo'].fillna('-')
-
-    # Remover as colunas extras
-    df_plan_eqp.drop(['Plano de manutenção', 'Grupo', 'EQP_N6'], axis=1, inplace=True)
-
-    #st.write()
-
-    df_plan_eqp['PLANO'].astype(str).apply(lambda x: ','.join(sorted(pd.unique(x.split(',')))))#*
-    df_plan_eqp['LT'].astype(str).apply(lambda x: ','.join(sorted(pd.unique(x.split(',')))))#*
-
-    #df_plan_eqp['PLANO'].astype(str).apply(lambda x: ','.join(sorted(set(x.replace('[]','').replace(',,',',').split(',')))))#*
-    #df_plan_eqp['LT'].astype(str).apply(lambda x: ','.join(sorted(set(x.replace('[]','').replace(',,',',').split(',')))))#*
-
-    # aaa
-
-    st.write(df_plan_eqp)
-
-    df_plan_eqp.drop_duplicates(subset='ID_SAP_N6')
-    df_plan_eqp.dropna(how='all')
-    df_plan_eqp.reset_index(drop=True)
-
-    st.session_state.df_plan_eqp = df_plan_eqp
-
-    df_plan_eqp = st.session_state['df_plan_eqp']
-
-    col1, col2 = st.columns([4, 2])
-
-    #   SETUP PLANTA E GRUPO
-
-    options_planta = col1.multiselect(
-        label='Selecione a(s) planta(s)',
-        options=sorted(list(df_plan_eqp['PLANTA'].unique()))
-        )
-
-    options_grupo = col1.multiselect(
-        label='Selecione o(s) grupo(s)',
-        options=sorted(list(df_plan_eqp['GRUPO'].unique()))
-        )
-
-    options_tem_plano = col1.multiselect(
-        label='Status plano',
-        options=['SIM', 'SEM PLANO'],
-        default='SEM PLANO'
-        )
-
-    #   GRÁFICOS
-
-    ## FAZER HEATMAP:
-
-    df_heatmap = pd.DataFrame({'PLANTA': [], '% CARREGADO': []})
-    for fabrica in list(df_plan_eqp['PLANTA'].unique()):
-        try:
-            count_tot = (df_plan_eqp['PLANTA'] == str(fabrica)).sum()
-            count_templano = ((df_plan_eqp['TEM PLANO?'] == 'SIM') & (df_plan_eqp['PLANTA'] == str(fabrica))).sum()
-            df_heatmap = pd.concat(
-                [df_heatmap, pd.DataFrame({'PLANTA': [str(fabrica)], '% CARREGADO': [count_templano / count_tot]})],
-                ignore_index=True, sort=False)
-        except:
-            pass
-
-    col2.subheader('% Equipamentos com Planos Carregados por Fábrica',anchor=False)
-    col2.table(df_heatmap.style.background_gradient(subset=["% CARREGADO"], cmap="RdYlGn", vmin=0, vmax=1))
+        # Concatene os DataFrames
+        eqp_tl_plan = pd.concat([eqp_tl_plan, novas_linhas], ignore_index=True)
 
 
-    ##   DISPLAY DE DATAFRAME COM FILTROS:
+        # Dicionário mapeando os valores de 'ID Equipamento' para 'ID_SAP_N6'
+        id_equipamento_to_id_sap_n6 = SAP_EQP_N6.set_index('ID_SAP_N6')['LI_N5'].to_dict()
 
-    if options_grupo is not None and options_planta is not None and options_tem_plano is not None:
-        with st.spinner('Carregando Gráficos...'):
-            df_plan_eqp_option = df_plan_eqp[df_plan_eqp['PLANTA'].isin(options_planta) & df_plan_eqp['GRUPO'].isin(options_grupo) & df_plan_eqp['TEM PLANO?'].isin(options_tem_plano)]
+        # Mapear 'ID Equipamento' para 'ID_SAP_N6' e, em seguida, associe os valores correspondentes de 'LI_N5'
+        #eqp_tl_plan_com_li = eqp_tl_plan.copy()  # Copie o DataFrame principal para manter sua integridade
+        eqp_tl_plan['LI_N5'] = eqp_tl_plan['ID Equipamento'].map(id_equipamento_to_id_sap_n6)
 
-            df_plan_eqp_option.drop_duplicates(subset=['ID_SAP_N6'])
-            df_plan_eqp_option.dropna(how='all')
-            df_plan_eqp_option.reset_index(drop=True)
 
-            col1.dataframe(df_plan_eqp_option)
+        #   CRIAR E FORMATAR DF COM BASE NA BASE DE EQUIPAMENTOS
 
-            # Salvando em arquivo excel
-            import io
-            buffer1 = io.BytesIO()
-            with pd.ExcelWriter(buffer1, engine="xlsxwriter") as excel_writer:
-                ## Crie um objeto ExcelWriter
-                nome_arquivo_sap = 'CHECK_PLXEQP'
+        df_plan_eqp = eqp_tl_plan
 
-                ## Salve cada DataFrame em uma planilha diferente
-                df_plan_eqp_option.to_excel(excel_writer, sheet_name='PLANO-TLxEQP', index=False)
+        df_plan_eqp['GRUPO'] = df_plan_eqp['LI_N5'].apply(lambda x: str(x)[9:12])
+        df_plan_eqp['PLANTA'] = df_plan_eqp['LI_N5'].apply(lambda x: str(x)[0:4])
 
-                ## Feche o objeto ExcelWriter
-                excel_writer.close()
+        df_plan_eqp['Plano de manutenção'] = df_plan_eqp['Plano de manutenção'].fillna('SEM ID')
+        df_plan_eqp['Grupo'] = df_plan_eqp['Grupo'].fillna('SEM ID')
+        df_plan_eqp['LI_N5'] = df_plan_eqp['LI_N5'].fillna('SEM LI')
 
-                col1.download_button(
-                    label="Download Relação PLANO/TL x EQP Filtrada",
-                    data=buffer1,
-                    file_name='PLANO-TLxEQP' + '.xlsx',
-                )
-                ###
+        df_plan_eqp['TEM PLANO/LISTA?'] = df_plan_eqp['Plano de manutenção'].apply(lambda x: 'SEM PLANO' if x == "SEM PLANO" else "CONTÉM")
+        df_plan_eqp['TEM PLANO/LISTA?'] = df_plan_eqp['Grupo'].apply(lambda x: 'SEM LISTA' if x == "SEM LISTA" else "CONTÉM")
+
+        df_plan_eqp = df_plan_eqp[df_plan_eqp['LI_N5'].str.contains('-IND-')]
+        df_plan_eqp.drop_duplicates(subset=None, inplace=True)  # Para remover todas as linhas duplicadas
+        df_plan_eqp.sort_values(by=['ID Equipamento','TEM PLANO/LISTA?'], inplace=True)      # Ordem com index ordenado com base na task list
+        df_plan_eqp.reset_index(drop=True, inplace=True)
+
+
+        st.session_state.df_plan_eqp = df_plan_eqp
+
+        df_plan_eqp = st.session_state['df_plan_eqp']
+
+        col1, col2 = st.columns([4, 2])
+
+        #   SETUP PLANTA E GRUPO
+
+        options_planta = col1.multiselect(
+            label='Selecione a(s) planta(s)',
+            options=sorted(list(df_plan_eqp['PLANTA'].unique()))
+            )
+
+        options_grupo = col1.multiselect(
+            label='Selecione o(s) grupo(s)',
+            options=sorted(list(df_plan_eqp['GRUPO'].unique()))
+            )
+
+        options_tem_plano = col1.multiselect(
+            label='Status plano',
+            options=['CONTÉM', 'SEM PLANO', 'SEM LISTA'],
+            default='SEM PLANO'
+            )
+
+        #   GRÁFICOS
+
+        ## FAZER HEATMAP:
+
+        df_heatmap = pd.DataFrame({'PLANTA': [], '% CARREGADO': []})
+        for fabrica in list(df_plan_eqp['PLANTA'].unique()):
+            try:
+                count_tot = (df_plan_eqp['PLANTA'] == str(fabrica)).sum()
+                count_templano = ((df_plan_eqp['TEM PLANO/LISTA?'] == 'CONTÉM') & (df_plan_eqp['PLANTA'] == str(fabrica))).sum()
+                df_heatmap = pd.concat(
+                    [df_heatmap, pd.DataFrame({'PLANTA': [str(fabrica)], '% CARREGADO': [count_templano / count_tot]})],
+                    ignore_index=True, sort=False)
+                df_heatmap.sort_values(by=['PLANTA'], inplace=True)
+                df_heatmap.reset_index(drop=True,inplace=True)
+            except:
+                pass
+
+        col2.subheader('% Equipamentos com Planos Carregados por Fábrica',anchor=False)
+        col2.table(df_heatmap.style.background_gradient(subset=["% CARREGADO"], cmap="RdYlGn", vmin=0, vmax=1))
+
+
+        ##   DISPLAY DE DATAFRAME COM FILTROS:
+
+        if options_grupo is not None and options_planta is not None and options_tem_plano is not None:
+            with st.spinner('Carregando Gráficos...'):
+                #df_plan_eqp_option = df_plan_eqp[df_plan_eqp['PLANTA'].isin(options_planta) & df_plan_eqp['GRUPO'].isin(options_grupo) & df_plan_eqp['Plano de manutenção'].isin(options_tem_plano) & df_plan_eqp['Grupo'].isin(options_tem_plano)]
+                df_plan_eqp_option = df_plan_eqp[
+                    (df_plan_eqp['PLANTA'].isin(options_planta)) &
+                    (df_plan_eqp['GRUPO'].isin(options_grupo)) &
+                    ((df_plan_eqp['Plano de manutenção'].isin(options_tem_plano)) |
+                    (df_plan_eqp['Grupo'].isin(options_tem_plano)) |
+                     df_plan_eqp['TEM PLANO/LISTA?'].isin(options_tem_plano))
+                    ]
+                col1.dataframe(df_plan_eqp_option)
+
+                # Salvando em arquivo excel
+                import io
+                buffer1 = io.BytesIO()
+                with pd.ExcelWriter(buffer1, engine="xlsxwriter") as excel_writer:
+                    ## Crie um objeto ExcelWriter
+                    nome_arquivo_sap = 'CHECK_PLXEQP'
+
+                    ## Salve cada DataFrame em uma planilha diferente
+                    df_plan_eqp_option.to_excel(excel_writer, sheet_name='PLANO-TLxEQP', index=False)
+
+                    ## Feche o objeto ExcelWriter
+                    excel_writer.close()
+
+                    col1.download_button(
+                        label="Download Relação PLANO/TL x EQP Filtrada",
+                        data=buffer1,
+                        file_name='PLANO-TLxEQP' + '.xlsx',
+                    )
+                    ###
 
 
 
