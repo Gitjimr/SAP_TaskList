04c087bb5908aebacab070cdf374b170a50efc8f
Gitjimr <166082003+Gitjimr@users.noreply.github.com>
2024-06-06 08:24:06 -0300

    Update 2_Plano_x_Equipamento.py

--- pages/2_Plano_x_Equipamento.py
+++ pages/2_Plano_x_Equipamento.py
@@ -56,6 +56,15 @@ st.markdown(
     unsafe_allow_html=True
 )
 
+prcnt_width = 80
+max_width_str = f"max-width: {prcnt_width}%;"
+st.markdown(f""" 
+            <style> 
+            .reportview-container .main .block-container{{{max_width_str}}}
+            </style>    
+            """,
+            unsafe_allow_html=True)
+
 import os
 
 path = os.path.dirname(__file__)
